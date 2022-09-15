52f6e8e50c38c3fd2858b4edaf4e300ce8be7441
Gina Sprint <sprint@gonzaga.edu>
2022-09-15 17:39:00 +0000

    finish crash course!

--- PythonBasicsFun/main.py
+++ PythonBasicsFun/main.py
@@ -1,4 +1,11 @@
 import csv
+import copy
+
+# subject.py with Subject
+# import subject
+# subject.Subject
+# from subject import Subject
+# Subject
 
 import hello_world
 
@@ -111,3 +118,123 @@ table = read_table("data.csv")
 print(table)
 # TASK: define/call write_table()
 write_table(table, "data_copy.csv")
+
+# warm up task
+def add_one(table):
+    for i in range(len(table)):
+        for j in range(len(table[i])):
+            table[i][j] += 1
+
+print("matrix before:", matrix)
+add_one(matrix)
+print("matrix after:", matrix)
+
+def clear_out(table):
+    table = []
+
+print("matrix before:", matrix)
+clear_out(matrix)
+print("matrix after:", matrix)
+
+# PYTHON IS PASS BY OBJECT REFERENCE
+# object refernces are passed by value (copied)
+# for more details: https://robertheaton.com/2014/02/09/pythons-pass-by-object-reference-as-explained-by-philip-k-dick/
+
+# SHALLOW VS DEEP COPY
+# shallow copy copies the object references not the objects themselves
+matrix_copy = matrix.copy() # shallow copy
+# deep copy copies the objects themselves
+matrix_deep_copy = copy.deepcopy(matrix) # deep copy
+
+print("matrix before:", matrix)
+print("matrix_copy before:", matrix_copy)
+print("matrix_deep_copy before:", matrix_deep_copy)
+add_one(matrix)
+print("matrix after:", matrix)
+print("matrix_copy after:", matrix_copy)
+print("matrix_deep_copy after:", matrix_deep_copy)
+
+# you probably want a deep copy
+
+# CLASSES
+# class: a collection of state and behavior that completely
+# describes something
+# object: an instance of a class
+class Subject:
+    """Represent a human subject in a research study.
+
+    Attributes:
+        sid(int): unique id number for the subject
+        name(str): name of the subject
+        measurements(dict or str:float): stores the timestamp:value
+            measurements collected from the subject during
+            the study
+
+        num_subjects(int): class-level attribute storing
+            the total number of subjects in the study
+    """
+    # declare your class-level attributes here
+    num_subjects = 0
+    # one num_subjects variable that is shared amongst all
+    # Subject objects
+    # do not declare instance-level attributes here
+
+    # special method __init__(self)
+    # like a constructor
+    def __init__(self, name, measurements=None):
+        # self is like the this reference
+        # self is a reference to the "current" or
+        # "invoking" object
+        self.sid = Subject.num_subjects
+        Subject.num_subjects += 1
+        self.name = name
+        if measurements is None:
+            measurements = {}
+        self.measurements = measurements
+    
+    # special method __str__
+    # invoked whenever a string represntation of
+    # an object is needed
+    def __str__(self):
+        return "SID: " + str(self.sid) + " NAME: " + self.name + \
+            " MEASUREMENTS: " + str(self.measurements)
+
+    # let's define our own instance-level method
+    def record_measurement(self, timestamp, value):
+        # should do error checking...
+        self.measurements[timestamp] = value
+    
+    # let's define our own class-level method
+    def display_num_subjects():
+        print("Number of subjects:", Subject.num_subjects)
+
+# lets create some Subject objects
+sub1 = Subject("cassidy")
+print(sub1)
+print(sub1.__str__())
+sub1.record_measurement("09-15-2022 10:21:00.00am", 1.1)
+print(sub1)
+print(sub1.measurements)
+
+sub2 = Subject("conan")
+print(sub2)
+Subject.display_num_subjects()
+
+# ASSERT STATEMENTS
+# assert something is true or false
+# if the assert statement evaluates to true, 
+# then execution continues
+# if the assert statement evaluates to false,
+# then execution stops
+assert 4 == 3
+print("here")
+
+# use assert statements to write unit tests
+# a unit test is a function that tests another function
+# for functional correctness
+# unit tests are comprised of several test cases
+# ranging from simple (e.g. common example, "happy path" example)
+# to complex (e.g. rare example) to edge cases 
+
+# assert statement operand ordering
+# actual vs expected (solution)
